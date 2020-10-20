3fe141f66f18ff5de75e87597ae40b51e4b8e3ab
Myron <dubtwee@gmail.com>
2020-10-20 12:39:22 -0500

    moving code organization for server.js

--- server/server.js
+++ server/server.js
@@ -29,8 +29,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 // Routes would go here
-let musicRouter = require('./routes/music_router');
-app.use('/musicLibrary', musicRouter);
+let musicLibrary = [
+  {
+    rank: 355,
+    artist: 'Ke$ha',
+    track: 'Tik-Toc',
+    published: '1/1/2009',
+  },
+  {
+    rank: 356,
+    artist: 'Gene Autry',
+    track: 'Rudolph, the Red-Nosed Reindeer',
+    published: '1/1/1949',
+  },
+  {
+    rank: 357,
+    artist: 'Oasis',
+    track: 'Wonderwall',
+    published: '1/1/1996',
+  },
+];
+
+app.get('/musicLibrary', (req, res) => {
+  res.send(musicLibrary);
+});
+
+app.post('/musicLibrary', (req, res) => {
+  musicLibrary.push(req.body);
+  res.sendStatus(201);
+});
+
+// MODULARIZING ROUTES
+// let musicRouter = require('./routes/music_router');
+// app.use('/musicLibrary', musicRouter);
 
 // Start express
 const PORT = 5000;
