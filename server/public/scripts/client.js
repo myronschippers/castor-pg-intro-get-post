2bf284edd06a55a4f0fc3054e57f19b102cdc67b
Myron <dubtwee@gmail.com>
2020-10-20 10:35:01 -0500

    code clean up with code styling and clear separation of concerns

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,48 +1,86 @@
 $(document).ready(onReady);
 
 function onReady() {
-    getMusicData();
-    $('#add').on('click', postMusicData);
+  $('#js-add').on('click', postMusicData);
+
+  getMusicData();
 }
+
+//
+// EVENT HANDLERS
+// ------------------------------
+
+function handleClickSave() {
+  let payloadObject = {
+    artist: $('#js-input-artist').val(),
+    track: $('#js-input-track').val(),
+    rank: $('#js-input-rank').val(),
+    published: $('#js-input-published').val(),
+  };
+
+  postMusicData(payloadObject);
+}
+
+//
+// API / SERVER INTERACTION
+// ------------------------------
 
 // get artist data from the server
 function getMusicData() {
-    $("#musicTableBody").empty();
-    $.ajax({
-        type: 'GET',
-        url: '/musicLibrary'
-    }).then(function (response) {
-        console.log("skjfhsd", response);
-        // append data to the DOM
-        for (let i = 0; i < response.length; i++) {
-            $('#musicTableBody').append(`
-                <tr>
-                    <td>${response[i].artist}</td>
-                    <td>${response[i].track}</td>
-                    <td>${response[i].rank}</td>
-                    <td>${response[i].published}</td>
-                </tr>
-            `);
-        }
+  $('#js-musicTableBody').empty();
+  $.ajax({
+    type: 'GET',
+    url: '/musicLibrary',
+  }).then(function (response) {
+    console.log('GET', response);
+
+    // render data to the DOM
+    render(response);
+  });
+}
+
+function postMusicData(payloadObject) {
+  $.ajax({
+    type: 'POST',
+    url: '/musicLibrary',
+    data: payloadObject,
+  })
+    .then(function (response) {
+      clearForm();
+
+      getMusicData();
+    })
+    .catch(function (err) {
+      console.log(err);
     });
 }
 
-function postMusicData() {
-    let payloadObject = {
-        artist: $('#artist').val(),
-        track: $('#track').val(),
-        rank: $('#rank').val(),
-        published: $('#published').val()
-    }
-    $.ajax({
-        type: 'POST',
-        url: '/musicLibrary',
-        data: payloadObject
-    }).then( function (response) {
-        $('#artist').val(''),
-        $('#track').val(''),
-        $('#rank').val(''),
-        $('#published').val('')
-        getMusicData();
-    });
+//
+// DOM INTERACTION / MANIPULATION
+// ------------------------------
+
+function clearForm() {
+  // clear fields
+  $('#js-input-artist').val('');
+  $('#js-input-track').val('');
+  $('#js-input-rank').val('');
+  $('#js-input-published').val('');
+}
+
+function render(musicLibrary) {
+  const $musicTableBody = $('#js-musicTableBody');
+
+  $musicTableBody.empty();
+  for (let i = 0; i < musicLibrary.length; i++) {
+    const musicData = musicLibrary[i];
+
+    $musicTableBody.append(`
+      <tr>
+        <td>${musicData.artist}</td>
+        <td>${musicData.track}</td>
+        <td>${musicData.rank}</td>
+        <td>${musicData.published}</td>
+      </tr>
+    `);
+  }
 }
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Setup body parser - to translating request body into JSON
-app.use( bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use( bodyParser.json() );
 app.use(express.static('server/public'));
 
@@ -12,10 +12,8 @@ app.use(express.static('server/public'));
 let musicRouter = require('./routes/music_router');
 app.use('/musicLibrary', musicRouter);
 
-
 // Start express
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log('up and running on port', PORT);
+  console.log('up and running on port', PORT);
 });
-
