a283a417f03a369ec94dfa10955d948d47a3e6dd
Myron <dubtwee@gmail.com>
2020-10-21 13:16:57 -0500

    ensuring PORT is configured for deployment

--- server/server.js
+++ server/server.js
@@ -9,33 +9,14 @@ app.use(bodyParser.json());
 app.use(express.static('server/public'));
 
 // Routes would go here
-// let musicLibrary = [
-//   {
-//     rank: 355,
-//     artist: 'Ke$ha',
-//     track: 'Tik-Toc',
-//     published: '1/1/2009',
-//   },
-//   {
-//     rank: 356,
-//     artist: 'Gene Autry',
-//     track: 'Rudolph, the Red-Nosed Reindeer',
-//     published: '1/1/1949',
-//   },
-//   {
-//     rank: 357,
-//     artist: 'Oasis',
-//     track: 'Wonderwall',
-//     published: '1/1/1996',
-//   },
-// ];
 
 // MODULARIZING ROUTES
 let musicRouter = require('./routes/musicLibrary.router');
 app.use('/musicLibrary', musicRouter);
 
 // Start express
-const PORT = 5000;
+// checking for environment variable PORT
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('up and running on port', PORT);
 });
