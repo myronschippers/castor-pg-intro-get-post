337063fd6c42f94a39944aa75ea4dfe16020d132
Myron <dubtwee@gmail.com>
2020-10-21 13:34:31 -0500

    adding the heroku DB config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,34 @@
 const pg = require('pg');
+const url = require('url');
 
-// DB Config Options
-const Pool = pg.Pool;
-const pool = new Pool({
+let dbConfig = {
   database: 'my-music-library', // name of our database
   host: 'localhost', // where is your database
   port: 5432,
   max: 10, // how many connections
   idleTimeoutMillis: 30000, // 30 second timeout
-});
+};
+
+// is heroku config available
+if (process.env.DATABASE_URL) {
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.split(':');
+
+  dbConfig = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true,
+    max: 10,
+    idleTimeoutMillis: 30000,
+  };
+}
+
+// DB Config Options
+const Pool = pg.Pool;
+const pool = new Pool(dbConfig);
 
 // check connection
 pool.on('connect', () => {
